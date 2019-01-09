5cf2c27785761e5b13152b6723aac17f1dc70a1d
Melika Kalbasi <melikalbasi@gmail.com>
2019-01-09 13:49:12 -0800

    fix package json issues

--- controllers/habbitrabbit_controller.js
+++ controllers/habbitrabbit_controller.js
@@ -55,7 +55,7 @@ router.get("/favrecipes", function(req,res){
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
-    res.json("/members");
+    res.json("/home");
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
