6b171d5db1fa72b40e707bca2aa67e7279e73b89
MDTrue <MDTrueII@hotmail.com>
2019-01-13 23:56:12 -0800

    navbar, footer and styling to home page

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
+    console.log("this is the user data==============================================="+user_data)
     $(".member-name").text(data.name);
   });
 });
