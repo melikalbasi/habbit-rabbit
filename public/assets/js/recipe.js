874f1493680ed15576da759d5f9a91366217ec8a
MDTrue <MDTrueII@hotmail.com>
2019-01-14 20:15:13 -0800

    update and revert to working good

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -70,68 +70,68 @@ $(document).ready(function() {
 
 
 // :: 5.0 Sliders Active Code
-if ($.fn.owlCarousel) {
-    var welcomeSlide = $('.hero-slides');
-    var receipeSlide = $('.receipe-slider');
+// if ($.fn.owlCarousel) {
+//     var welcomeSlide = $('.hero-slides');
+//     var receipeSlide = $('.receipe-slider');
 
-    welcomeSlide.owlCarousel({
-        items: 1,
-        margin: 0,
-        loop: true,
-        nav: true,
-        navText: ['Prev', 'Next'],
-        dots: true,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        smartSpeed: 1000
-    });
+//     welcomeSlide.owlCarousel({
+//         items: 1,
+//         margin: 0,
+//         loop: true,
+//         nav: true,
+//         navText: ['Prev', 'Next'],
+//         dots: true,
+//         autoplay: true,
+//         autoplayTimeout: 5000,
+//         smartSpeed: 1000
+//     });
 
-    welcomeSlide.on('translate.owl.carousel', function () {
-        var slideLayer = $("[data-animation]");
-        slideLayer.each(function () {
-            var anim_name = $(this).data('animation');
-            $(this).removeClass('animated ' + anim_name).css('opacity', '0');
-        });
-    });
+//     welcomeSlide.on('translate.owl.carousel', function () {
+//         var slideLayer = $("[data-animation]");
+//         slideLayer.each(function () {
+//             var anim_name = $(this).data('animation');
+//             $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+//         });
+//     });
 
-    welcomeSlide.on('translated.owl.carousel', function () {
-        var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
-        slideLayer.each(function () {
-            var anim_name = $(this).data('animation');
-            $(this).addClass('animated ' + anim_name).css('opacity', '1');
-        });
-    });
+//     welcomeSlide.on('translated.owl.carousel', function () {
+//         var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+//         slideLayer.each(function () {
+//             var anim_name = $(this).data('animation');
+//             $(this).addClass('animated ' + anim_name).css('opacity', '1');
+//         });
+//     });
 
-    $("[data-delay]").each(function () {
-        var anim_del = $(this).data('delay');
-        $(this).css('animation-delay', anim_del);
-    });
+//     $("[data-delay]").each(function () {
+//         var anim_del = $(this).data('delay');
+//         $(this).css('animation-delay', anim_del);
+//     });
 
-    $("[data-duration]").each(function () {
-        var anim_dur = $(this).data('duration');
-        $(this).css('animation-duration', anim_dur);
-    });
+//     $("[data-duration]").each(function () {
+//         var anim_dur = $(this).data('duration');
+//         $(this).css('animation-duration', anim_dur);
+//     });
 
-    var dot = $('.hero-slides .owl-dot');
-    dot.each(function () {
-        var index = $(this).index() + 1 + '.';
-        if (index < 10) {
-            $(this).html('0').append(index);
-        } else {
-            $(this).html(index);
-        }
-    });
+//     var dot = $('.hero-slides .owl-dot');
+//     dot.each(function () {
+//         var index = $(this).index() + 1 + '.';
+//         if (index < 10) {
+//             $(this).html('0').append(index);
+//         } else {
+//             $(this).html(index);
+//         }
+//     });
 
-    receipeSlide.owlCarousel({
-        items: 1,
-        margin: 0,
-        loop: true,
-        nav: true,
-        navText: ['Prev', 'Next'],
-        dots: true,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        smartSpeed: 1000
-    });
-}
+//     receipeSlide.owlCarousel({
+//         items: 1,
+//         margin: 0,
+//         loop: true,
+//         nav: true,
+//         navText: ['Prev', 'Next'],
+//         dots: true,
+//         autoplay: true,
+//         autoplayTimeout: 5000,
+//         smartSpeed: 1000
+//     });
+// }
 
